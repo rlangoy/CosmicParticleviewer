f56db0c9fc203606833dcc00e224db34f5dbae45
Rune Langøy <rune.langoy@gmail.com>
2022-10-20 13:30:46 +0200

    Bug Fix

--- Python/plotAplideHitmap.py
+++ Python/plotAplideHitmap.py
@@ -1,19 +1,20 @@
 from picoif import OuterBarriel
 
-serialPort='COM4'  #'COM95
+serialPort='COM95'  #'COM95
 print ("Open Serial port ",serialPort)
 alpide=OuterBarriel(serialPort)  
 
-print('Reset All ALPIDEs')
-alpide.sendReset()
+#print('Reset All ALPIDEs')
+#alpide.sendReset()
 
-ALPIDE_REG_VTEMP=0x0609
-print('Write to 0x1234 a  ALPIDE 0x70 Resister ALPIDE_REG_VTEMP')
+#ALPIDE_REG_VTEMP=0x0609
+#print('Write to 0x1234 a  ALPIDE 0x70 Resister ALPIDE_REG_VTEMP')
 
-alpide.writeRegister(0x70,ALPIDE_REG_VTEMP,0x1234)
-print("VCASN2 - Read: 0x%X"% myAlpides.readRegister(0x70,VCASN2))
+#alpide.writeRegister(0x70,ALPIDE_REG_VTEMP,0x1234)
+#print("ALPIDE_REG_VTEMP - Read: 0x%X"% alpide.readRegister(0x70,ALPIDE_REG_VTEMP))
 
-print('send Trigger signal to all ALPIDEs')
+
+print('send Trigger & Reset signal to all ALPIDEs')
 alpide.sendTrigger()
 
 alpide.plotAllChips()
