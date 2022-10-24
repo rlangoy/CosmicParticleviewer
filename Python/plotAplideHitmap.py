9529d7dd31b15392159a452f9bdbc1160a69a29c
Rune Langøy <rune.langoy@gmail.com>
2022-10-24 13:40:28 +0200

    Cleanup

--- Python/plotAplideHitmap.py
+++ Python/plotAplideHitmap.py
@@ -8,9 +8,9 @@ alpide=OuterBarriel(serialPort)
 #alpide.sendReset()
 
 #ALPIDE_REG_VTEMP=0x0609
-#print('Write to 0x1234 a  ALPIDE 0x70 Resister ALPIDE_REG_VTEMP')
+#print('Write to 0x12 a  ALPIDE 0x70 Resister ALPIDE_REG_VTEMP')
 
-#alpide.writeRegister(0x70,ALPIDE_REG_VTEMP,0x1234)
+#alpide.writeRegister(0x70,ALPIDE_REG_VTEMP,0x12)
 #print("ALPIDE_REG_VTEMP - Read: 0x%X"% alpide.readRegister(0x70,ALPIDE_REG_VTEMP))
 
 
